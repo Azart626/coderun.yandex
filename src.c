37f65dc7cbb58754619ff7bf84ece7b2e19d5d57
Azart <qerefdany201335@gmail.com>
2024-11-04 00:02:04 +0300

    task1

--- src.c
+++ src.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 int main(){
-    int a,b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a+b);
+    int n = 2;
+    int arr[n];
+    int temp = 0;
+    scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+    if (arr[0] > 1000 || arr[0] < -1000 || arr[1] > 1000 || arr[1] < -1000 || arr[2] > 1000 || arr[2] < -1000)
+    {
+        printf("n/a");
+        return 0;
+    } 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }            
+
+        }
+        
+    }
+    printf("%d %d %d", arr[0], arr[1], arr[2]);
+    printf("%d", arr[1]);
+    
 }
